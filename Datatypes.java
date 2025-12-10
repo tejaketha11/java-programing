64faace0b0571461869d7b5c5febada6de1f4258
tejaketha11 <kethanagateja9@gmail.com>
2025-12-10 15:34:06 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+class Datatypes{
+public static void main(String[] args){
+int i=10;
+byte b=15;
+short s=100;
+long l=1555L;
+float f = 20.5f;
+double d = 10.5;
+char c ='A';
+boolean bool = true;
+ System.out.println("Int:" + i);
+System.out.println("Byte:" + b);
+System.out.println("Short:" + s);
+System.out.println("Long:" + l);
+System.out.println("Float:" + f);
+System.out.println("Double:" + d);
+System.out.println("Char:" + c);
+System.out.println("Boolean:" + bool);
+}
+}
---
+++ Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables{
+    public static void main(String[] args) {
+        int age = 20;
+        double price = 19.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Alice";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
